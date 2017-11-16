4ec33a6c85fe62f40ec54aa1d9d1c492de7a0ca9
Qwert26 <qwert268862@gmail.com>
2017-11-16 15:00:47 +0100

    Winkelmessung
    Winkelgeschwindigkeit und seine Ableitungen werden jetzt auch gemessen.

--- IndustrialEnterpriseUpgrade/Codebasis/IndustrialEnterpriseUpgrade/Movement/KinematicMeasure.cs
+++ IndustrialEnterpriseUpgrade/Codebasis/IndustrialEnterpriseUpgrade/Movement/KinematicMeasure.cs
@@ -4,18 +4,11 @@ namespace IndustrialEnterpriseUpgrade.Movement {
 	public class KinematicMeasure : Block {
 		private static string[] NAMES = new string[] {"velocity","acceleration","jerk","jounce"};
 		private const string INFINITY = "Unlimited";
-		private Vector3[] lastMeasurements=new Vector3[NAMES.Length];
-		private float[] limits = new float[NAMES.Length];
+		private Vector3[] lastLinearMeasurements=new Vector3[NAMES.Length];
+		private Vector3[] lastAngularMeasurements = new Vector3[NAMES.Length];
+		private float[] linearLimits = new float[NAMES.Length];
+		private float[] angularLimits = new float[NAMES.Length];
 		private Vector2 scrollPosition;
-		public override void BlockStart() {
-			base.BlockStart();
-			for (int i = 0; i < limits.Length; i++) {
-				limits[i] = item.Code.Variables.GetFloat(NAMES[i]+"limit",float.PositiveInfinity);
-				if (limits[i] < 0) {
-					limits[i] = float.PositiveInfinity;
-				}
-			}
-		}
 		public override void StateChanged(IBlockStateChange change) {
 			base.StateChanged(change);
 			if (change.IsAvailableToConstruct) {
@@ -25,12 +18,16 @@ namespace IndustrialEnterpriseUpgrade.Movement {
 			}
 		}
 		private void Measure(float deltaTime) {
-			Vector3[] currentMeasurements = new Vector3[NAMES.Length];
-			currentMeasurements[0] = MainConstruct.iPhysics.iVelocities.VelocityVector;
+			Vector3[] currentLinearMeasurements = new Vector3[NAMES.Length];
+			currentLinearMeasurements[0] = MainConstruct.iPhysics.iVelocities.VelocityVector;
+			Vector3[] currentAngularMeasurements = new Vector3[NAMES.Length];
+			currentAngularMeasurements[0] = MainConstruct.iPhysics.iVelocities.AngularVelocity;
 			for (int i=1;i<NAMES.Length;i++) {
-				currentMeasurements[i] = (currentMeasurements[i - 1] - lastMeasurements[i - 1]) / deltaTime;
+				currentLinearMeasurements[i] = (currentLinearMeasurements[i - 1] - lastLinearMeasurements[i - 1]) / deltaTime;
+				currentAngularMeasurements[i] = (currentAngularMeasurements[i - 1] - lastAngularMeasurements[i - 1]) / deltaTime;
 			}
-			lastMeasurements = currentMeasurements;
+			lastLinearMeasurements = currentLinearMeasurements;
+			lastAngularMeasurements = currentAngularMeasurements;
 		}
 		#region Nutzerinteraktion
 		public override InteractionReturn Secondary() {
@@ -40,34 +37,57 @@ namespace IndustrialEnterpriseUpgrade.Movement {
 			};
 			ret.AddExtraLine("Press <<Q>> to open the configuration GUI.");
 			for (int i = 0; i < NAMES.Length; i++) {
-				if (limits[i] * limits[i] <= lastMeasurements[i].sqrMagnitude) {
-					ret.AddExtraLine("<!current " + NAMES[i] + " is " + lastMeasurements[i] + "!>");
+				string line;
+				if (linearLimits[i] * linearLimits[i] <= lastLinearMeasurements[i].sqrMagnitude) {
+					line = "Current <!linear " + NAMES[i] + " is " + lastLinearMeasurements[i] + "!>";
 				} else {
-					ret.AddExtraLine("current "+NAMES[i]+" is "+lastMeasurements[i]);
+					line = "Current linear " + NAMES[i] + " is " + lastLinearMeasurements[i];
 				}
+				line += ", while ";
+				if (angularLimits[i] * angularLimits[i] <= lastAngularMeasurements[i].sqrMagnitude) {
+					line += "<!angular " + NAMES[i] + " is " + lastAngularMeasurements[i] + "!>.";
+				} else {
+					line += "angular " + NAMES[i] + " is " + lastAngularMeasurements[i] + ".";
+				}
+				ret.AddExtraLine(line);
 			}
 			return ret;
 		}
 		public override BlockTechInfo GetTechInfo() {
-			return new BlockTechInfo().AddStatement("Measures current velocity, acceleration, jerk and jounce vectors.");
+			return new BlockTechInfo().AddStatement("Measures current linear velocity, acceleration, jerk and jounce vectors.").
+				AddStatement("Also measures the angular variations of those.");
 		}
 		public override void Secondary(Transform T){
 			new GenericBlockGUI().ActivateGui(this);
 		}
 		public override bool ExtraGUI() {
 			GUILayout.BeginArea(new Rect(0,0,320,800),"Limit settings",GUI.skin.window);
-			scrollPosition=GUILayout.BeginScrollView(scrollPosition,false,true);
+			scrollPosition=GUILayout.BeginScrollView(scrollPosition,true,true);
 			for (int i = 0; i < NAMES.Length; i++) {
-				GUILayout.Box("Limit for "+NAMES[i]+":");
+				GUILayout.BeginHorizontal();
+				GUILayout.Box("Limit for linear " + NAMES[i] + ":");
+				GUILayout.Box("Limit for angular " + NAMES[i] + ":");
+				GUILayout.EndHorizontal();
+				GUILayout.BeginHorizontal();
 				string input;
-				if (float.IsInfinity(limits[i])) {
+				if (float.IsInfinity(linearLimits[i])) {
 					input = GUILayout.TextField(INFINITY);
 				} else {
-					input = GUILayout.TextField(limits[i].ToString());
+					input = GUILayout.TextField(linearLimits[i].ToString());
 				}
-				if (!float.TryParse(input, out limits[i])) {
-					limits[i] = float.PositiveInfinity;
+				if (!float.TryParse(input, out linearLimits[i])) {
+					linearLimits[i] = float.PositiveInfinity;
 				}
+
+				if (float.IsInfinity(angularLimits[i])) {
+					input = GUILayout.TextField(INFINITY);
+				} else {
+					input = GUILayout.TextField(angularLimits[i].ToString());
+				}
+				if (!float.TryParse(input, out angularLimits[i])) {
+					angularLimits[i] = float.PositiveInfinity;
+				}
+				GUILayout.EndHorizontal();
 			}
 			GUILayout.EndScrollView();
 			bool ret = false;
