ad98bb010b19b595cf601d538831d5c07205197f
Qwert26 <qwert268862@gmail.com>
2018-03-08 12:50:45 +0100

    V2.16 Recompile
    Implementierung von FTDPlugin_PostLoad

--- IndustrialEnterpriseUpgrade/Codebasis/IndustrialEnterpriseUpgrade/IndustrialEnterpriseUpgradePlugin.cs
+++ IndustrialEnterpriseUpgrade/Codebasis/IndustrialEnterpriseUpgrade/IndustrialEnterpriseUpgradePlugin.cs
@@ -1,9 +1,18 @@
 ﻿using System;
 using UnityEngine;
 namespace IndustrialEnterpriseUpgrade {
-	public class IndustrialEnterpriseUpgradePlugin : FTDPlugin {
+	public class IndustrialEnterpriseUpgradePlugin : FTDPlugin, FTDPlugin_PostLoad {
 		public string name => "Industrial Enterprise Upgrade";
-		public Version version => new Version(2,15,0,0);
+		public Version version => new Version(2,16,0,0);
+		/// <summary>
+		/// Lädt weitere Daten nach, sobald das Spiel sämtliche Plug-Ins geladen hat.
+		/// </summary>
+		/// <returns>
+		/// true
+		/// </returns>
+		public bool AfterAllPluginsLoaded() {
+			return true;
+		}
 		/// <summary>
 		/// Das Plugin wird geladen und sollte notwendige Vorbereitungen treffen.
 		/// </summary>
